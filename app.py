f15d7e2f798d51f5f49dae3efda82a91d8ff4dab
Ryan Hemmila <ryanhemmila@gmail.com>
2021-11-09 18:26:07 -0600

    added cards

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 
+import psycopg2 as psql
+
 app = Flask(__name__)
+conn = psql.connect("dbname=faces user=admin password=admin")
+
+def get_image_count():
+  cursor = conn.cursor()
+  cursor.execute("""SELECT COUNT(*) FROM images;""")
+  count = cursor.fetchone()[0]
+  cursor.close()
+  return count
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    return render_template('index.html',image_count=get_image_count())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
