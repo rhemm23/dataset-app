a0d0d0e8817beab3f4bc9fd71ec2da358f14b53f
root <root@instance-1.us-central1-a.c.capstone-project-330614.internal>
2021-11-10 00:17:26 +0000

    added resources

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello there!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
