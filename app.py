1305acee03026001457792ba539ca3e64fe5c30c
Ryan Hemmila <ryanhemmila@gmail.com>
2021-11-09 20:01:19 -0600

    f

--- app.py
+++ app.py
@@ -14,20 +14,11 @@ def get_image_count():
   return count
 
 def get_original_images(page):
-
-  image_count = get_image_count()
-  page_count = math.ceil(image_count / 50)
-
-  if page >= page_count:
-    page = page_count - 1
-  elif page < 0:
-    page = 0
-
   cursor = conn.cursor()
   cursor.execute("""SELECT id, width, height, google_file_name FROM images ORDER BY id asc LIMIT 50 OFFSET %s;""", (page * 50,))
   images = cursor.fetchall()
   cursor.close()
-  return images, page_count
+  return images
 
 @app.route('/original-images')
 def original_images():
@@ -42,11 +33,20 @@ def original_images():
     except:
       page = 0
 
-  images, page_count = get_original_images(page)
+  image_count = get_image_count()
+  page_count = math.ceil(image_count / 50)
+
+  if page >= page_count:
+    page = page_count - 1
+  elif page < 0:
+    page = 0
+
+  images = get_original_images(page)
 
   return render_template(
     'original_images.html',
     title='Original Images',
+    page=page,
     images=images,
     page_count=page_count
   )
